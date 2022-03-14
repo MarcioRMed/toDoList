af981bf1e35d63953a4c0d565395cf5672f1894d
MarcioRMed <contato.microinfo@gmail.com>
2022-03-14 18:18:29 -0300

    deploy configs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // importando modulos- como se fosse
+require('dotenv').config() // variaveis de ambiente com servidor
 const express = require("express");
 const path = require("path");
 const routes = require("./routes/routes");
@@ -6,7 +7,7 @@ const connectToDb = require("./database/db");
 
 connectToDb();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;  //config da porta do servidor externo
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public"))); //configuração funcionar o css, javascript
@@ -17,5 +18,13 @@ app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
 );
 
+
+
+
+
+
+
 // alt + swif + f -> adiciona automaticamente a pontuação e organiza o códido - funcão do Pretty extensão
 // importante o ; no node - evita erros
+
+// Ctrl+j - abre o terminal 
